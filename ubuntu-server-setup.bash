4a89fcd3e430c94adbfa442787e16a4b17907f66
Micah Zoltu <micah@zoltu.net>
2024-04-04 18:06:59 +0800

    Update ubuntu-server-setup.bash

--- ubuntu-server-setup.bash
+++ ubuntu-server-setup.bash
@@ -11,8 +11,8 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
-# install latest version of docker via apt
-apt-get install --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# install docker v25.0.5-1 via apt package manager
+apt-get install docker-ce=5:25.0.5-1~ubuntu.22.04~jammy docker-ce-cli=5:25.0.5-1~ubuntu.22.04~jammy containerd.io docker-buildx-plugin docker-compose-plugin
 
 # start docker
 systemctl start docker
